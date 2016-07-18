4cabe15cf87101c879ee92e8451afd3e11c05703
Luis Santos <luis.santos@nfsconsultoria.com.br>
2016-07-18 16:22:52 -0300

    Alteração 2 do projeto

--- src/main/java/br/com/nfsconsultoria/azcontrole/bean/FornecedorBean.java
+++ src/main/java/br/com/nfsconsultoria/azcontrole/bean/FornecedorBean.java
@@ -2,12 +2,33 @@ package br.com.nfsconsultoria.azcontrole.bean;
 
 import br.com.nfsconsultoria.azcontrole.dao.FornecedorDAO;
 import br.com.nfsconsultoria.azcontrole.domain.Fornecedor;
+import com.lowagie.text.BadElementException;
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.Paragraph;
+import com.lowagie.text.Rectangle;
+import java.io.File;
+import java.io.IOException;
 import org.omnifaces.util.Messages;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.event.ActionEvent;
 import java.util.List;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
+import org.apache.poi.hssf.usermodel.HSSFCell;
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
+import org.apache.poi.hssf.usermodel.HSSFFont;
+import org.apache.poi.hssf.usermodel.HSSFRow;
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.hssf.util.HSSFColor;
+import org.apache.poi.ss.usermodel.CellStyle;
 
 /**
  * @author luissantos
@@ -88,4 +109,52 @@ public class FornecedorBean {
             erro.printStackTrace();
         }
     }
+    
+    public void preProcessPDF(Object document) throws IOException, BadElementException, DocumentException {
+        Document pdf = (Document) document;
+        Rectangle size = new Rectangle(1900, 600);
+        pdf.addAuthor("Luis Carlos Santos");
+        pdf.addTitle("Dentistas Cadastrados");
+        pdf.addCreator("NFS Consultoria");
+        pdf.addSubject("Dentistas Cadastrados");
+        pdf.setPageSize(PageSize.A4.rotate());
+        pdf.setMarginMirroring(true);
+
+        pdf.open();
+
+        Font catFont = new Font(Font.TIMES_ROMAN, 18, Font.BOLD);
+
+        Paragraph p = new Paragraph("Relatório de Dentistas", catFont);
+        p.setAlignment(Element.ALIGN_CENTER);
+        p.setSpacingAfter(20);
+
+        ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+        String logo = externalContext.getRealPath("") + File.separator + "resources" + File.separator + "images" + File.separator + "banner.png";
+
+        pdf.add(Image.getInstance(logo));
+        pdf.add(p);
+    }
+
+    public void postProcessXLS(Object document) {
+        HSSFWorkbook wb = (HSSFWorkbook) document;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        HSSFRow header = sheet.getRow(0);
+
+        HSSFFont font = wb.createFont();
+        font.setBold(true);
+        font.setColor(HSSFColor.WHITE.index);
+
+        HSSFCellStyle cellStyle = wb.createCellStyle();
+        cellStyle.setFillForegroundColor(HSSFColor.LIGHT_BLUE.index);
+        cellStyle.setFillPattern(HSSFCellStyle.SOLID_FOREGROUND);
+        cellStyle.setWrapText(true);
+        cellStyle.setAlignment(CellStyle.ALIGN_JUSTIFY);
+        cellStyle.setFont(font);
+
+        for (int i = 0; i < header.getPhysicalNumberOfCells(); i++) {
+            HSSFCell cell = header.getCell(i);
+
+            cell.setCellStyle(cellStyle);
+        }
+    }    
 }
