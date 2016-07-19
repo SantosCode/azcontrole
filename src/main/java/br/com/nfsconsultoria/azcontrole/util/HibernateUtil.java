686d16d16187c174ba3d50b444986e0dc7ee2e99
Luis Santos <luis.santos@nfsconsultoria.com.br>
2016-07-19 12:40:20 -0300

    Hibernate Alterado

--- src/main/java/br/com/nfsconsultoria/azcontrole/util/HibernateUtil.java
+++ src/main/java/br/com/nfsconsultoria/azcontrole/util/HibernateUtil.java
@@ -1,32 +1,56 @@
 package br.com.nfsconsultoria.azcontrole.util;
 
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.jdbc.ReturningWork;
 import org.hibernate.service.ServiceRegistry;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
- * Created by luissantos on 06/06/16.
- * @author luissantos
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author luis
  */
 public class HibernateUtil {
-    private static final SessionFactory SESSION_FACTORY = criarFabricaDeSesoes();
+
+    private static SessionFactory fabricaDeSessoes = criarFabricaDeSessoes();
 
     public static SessionFactory getFabricaDeSessoes() {
-        return SESSION_FACTORY;
+        return fabricaDeSessoes;
     }
 
-    private static SessionFactory criarFabricaDeSesoes() {
+
+    /* Converter sessão para conexão para relatório do jasper */
+    public static Connection getConexao() {
+        Session sessao = fabricaDeSessoes.openSession();
+
+        Connection conexao = sessao.doReturningWork(new ReturningWork<Connection>() {
+            @Override
+            public Connection execute(Connection conn) throws SQLException {
+                return conn;
+            }
+        });
+
+        return conexao;
+    }
+
+    private static SessionFactory criarFabricaDeSessoes() {
         try {
-            Configuration configuration = new Configuration().configure();
-            ServiceRegistry registro = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties()).build();
-            SessionFactory fabrica = configuration.buildSessionFactory(registro);
+            Configuration configuracao = new Configuration().configure();
+
+            ServiceRegistry registro = new StandardServiceRegistryBuilder().applySettings(configuracao.getProperties()).build();
+
+            SessionFactory fabrica = configuracao.buildSessionFactory(registro);
+
             return fabrica;
         } catch (Throwable ex) {
-            System.err.println("A fabrica de sessões não pode ser criada. " + ex);
+            System.err.println("A fábrica de sessões não pode ser criada." + ex);
             throw new ExceptionInInitializerError(ex);
         }
     }
-
 }
